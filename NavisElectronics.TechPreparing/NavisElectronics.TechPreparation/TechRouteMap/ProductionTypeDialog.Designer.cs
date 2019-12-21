f287bcd9fbc6b8794015ee1ce21797e9231a3c40
RegressX <redisthepunk@gmail.com>
2019-12-21 18:20:57 +0300

    Добавил окно с диалогом выбора типа производства

---
+++ NavisElectronics.TechPreparing/NavisElectronics.TechPreparation/TechRouteMap/ProductionTypeDialog.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace NavisElectronics.TechPreparation.TechRouteMap
+{
+    partial class ProductionTypeDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "Общий",
+            "Цех НАП",
+            "Цех микроэлектроники"});
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(268, 251);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listBox1_MouseDoubleClick);
+            // 
+            // ProductionTypeDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 273);
+            this.Controls.Add(this.listBox1);
+            this.Name = "ProductionTypeDialog";
+            this.Text = "ProductionTypeDialog";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
---
+++ NavisElectronics.TechPreparing/NavisElectronics.TechPreparation/TechRouteMap/ProductionTypeDialog.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NavisElectronics.TechPreparation.TechRouteMap
+{
+    public partial class ProductionTypeDialog : Form
+    {
+        public ProductionTypeDialog()
+        {
+            InitializeComponent();
+        }
+
+        public string ProductionTypeValue { get; set; }
+
+        private void listBox1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                if (listBox1.SelectedItem != null)
+                {
+                    DialogResult = DialogResult.OK;
+                    ProductionTypeValue = (string) listBox1.SelectedItem;
+                    this.Close();
+                }
+            }
+        }
+    }
+}
