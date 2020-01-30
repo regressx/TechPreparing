152284c4a44120b84c052e7e9346a46dc3d4f0a0
RegressX <redisthepunk@gmail.com>
2020-01-30 15:25:32 +0300

    Избавился от ненужного пространства имен

--- NavisElectronics.TechPreparing/NavisElectronics.TechPreparing.Data/IntermechWriter.cs
+++ NavisElectronics.TechPreparing/NavisElectronics.TechPreparing.Data/IntermechWriter.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Runtime.Serialization.Formatters.Binary;
 using System.Threading.Tasks;
 using Intermech;
 using Intermech.Interfaces;
