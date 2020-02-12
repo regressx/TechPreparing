343c81a76e6b0dcd07d7a35126928da96f17adce
RegressX <redisthepunk@gmail.com>
2020-02-12 19:30:00 +0300

    Чтение атрибутов из формул

--- NavisElectronics.TechPreparing/NavisElectronics.TechPreparation/Services/RateService.cs
+++ NavisElectronics.TechPreparing/NavisElectronics.TechPreparation/Services/RateService.cs
@@ -1,5 +1,9 @@
 ﻿using System.Collections.Generic;
+using System.Data;
 using System.Globalization;
+using Intermech.Interfaces;
+using Intermech.Interfaces.Compositions;
+using Intermech.Kernel.Search;
 //using Jace;
 //using Jace.Execution;
 //using Jace.Operations;
@@ -10,7 +14,7 @@ using NavisElectronics.TechPreparation.Interfaces.Entities;
 namespace NavisElectronics.TechPreparation.Services
 {
     /// <summary>
-    /// Сервис позволяет получить норму расхода материала на операцию
+    /// Сервис-фасад. Позволяет получить норму расхода материала на операцию
     /// </summary>
     public class RateService
     {
@@ -21,17 +25,106 @@ namespace NavisElectronics.TechPreparation.Services
             //_calculationEngine = calculationEngine;
         }
 
-        public ModeOperationCatalogNode Find(RateCatalog rateCatalog,
+        public MeasuredValue Find(RateCatalog rateCatalog,
                                              string materialName,
                                              string operationName,
-                                             string operationMode)
+                                             string operationMode,
+                                             long operationVersionId)
         {
+
+            // если всё правильно организовать, то поиск за log(n)
             MaterialCatalogNode material = (MaterialCatalogNode)rateCatalog.FindMaterial(materialName);
             OperationCatalogNode operationCatalog = material.FindOperation(operationName);
             ModeOperationCatalogNode operationModeCatalogNode = operationCatalog.FindOperationMode(operationMode);
-            return operationModeCatalogNode;
+
+
+            // получить параметры из формулы
+            IEnumerable<string> parametersToFind = ParametersParser.GetParameters(operationModeCatalogNode.FormulaText);
+
+            //здесь выбираем, откуда нужно забирать данные
+            switch (operationModeCatalogNode.ActionType)
+            {
+                case ActionType.GetAttributeFromOneObject:
+
+                    // получить тип, который надо искать. Это может быть или операция, или заготовка
+                    int type = operationModeCatalogNode.ObjectTypeToCalculateAttribute;
+
+                    long objectTypeId = FindBlank(operationVersionId);
+
+                    // найти у этого типа атрибут и его значение
+                    foreach (string parameter in parametersToFind)
+                    {
+                        int attributeId = MetaDataHelper.GetAttributeByTypeNameID(parameter);
+                        using (SessionKeeper keeper = new SessionKeeper())
+                        {
+                            IDBObject blankObject = keeper.Session.GetObject(objectTypeId);
+                            IDBAttribute currentAttribute = blankObject.GetAttributeByID(attributeId);
+
+                        }
+                    }
+
+                    break;
+            }
         }
 
+        private long FindBlank(long operationVersionId)
+        {
+            // подняться вверх до маршрута обработки
+            using (SessionKeeper keeper = new SessionKeeper())
+            {
+                // Сервис для получения составов
+                ICompositionLoadService compositionService =
+                    (ICompositionLoadService)keeper.Session.GetCustomService(typeof(ICompositionLoadService));
+
+                int type = -1;
+                long routeId = operationVersionId;
+
+                // крутим, пока не найдем маршрут обработки
+                while (type != 1037)
+                {
+                    DataTable table = compositionService.LoadCompositionApplicability(keeper.Session, routeId, 1002, columnsForTechRoutes,
+                        Intermech.SystemGUIDs.filtrationBaseVersions, 1117, 1037, 1110);
+
+                    foreach (DataRow row in table.Rows)
+                    {
+                        routeId = (long)row[0];
+                        type = (int)row[1];
+                    }
+
+                    if (table.Rows.Count != 1)
+                    {
+                        break;
+                    }
+                }
+
+                if (type != 1037)
+                {
+                    throw new System.Exception("У операции нет родителя в виде маршрута обработки!");
+                }
+
+                ColumnDescriptor[] columnsForBlank =
+                {
+                    new ColumnDescriptor((int) ObligatoryObjectAttributes.F_OBJECT_ID, AttributeSourceTypes.Object,
+                        ColumnContents.Text, ColumnNameMapping.Index, SortOrders.NONE, 0), // идентификатор версии объекта
+                    new ColumnDescriptor((int) ObligatoryObjectAttributes.F_OBJECT_TYPE, AttributeSourceTypes.Object,
+                        ColumnContents.Text, ColumnNameMapping.Index, SortOrders.NONE, 0), // тип объекта
+                    new ColumnDescriptor(1220, AttributeSourceTypes.Object,
+                    ColumnContents.Text, ColumnNameMapping.Index, SortOrders.NONE, 0) // вид изделия
+                };
+
+
+                // Поиск состава
+                DataTable blankTable = compositionService.LoadComposition(keeper.Session, routeId, 1002, new List<ColumnDescriptor>(columnsForBlank), Intermech.SystemGUIDs.filtrationBaseVersions, 1090); // забрать заготовку
+
+                // проверим и оповестим пользователя, что отсутствует заготовка
+                if (blankTable.Rows.Count == 0)
+                {
+                    throw new System.Exception("У Вас отсутствует объект типа Заготовка в составе маршрута обработки");
+                }
+
+                return (long)blankTable.Rows[0][0];
+            }
+        }
     }
 
     public class MaterialCatalogNode : RateCatalogNode
@@ -44,7 +137,6 @@ namespace NavisElectronics.TechPreparation.Services
 
     public class OperationCatalogNode : RateCatalogNode
     {
-        public string Name { get; set; }
         public ModeOperationCatalogNode FindOperationMode(string operationMode)
         {
             return (ModeOperationCatalogNode)base.Find(operationMode);
