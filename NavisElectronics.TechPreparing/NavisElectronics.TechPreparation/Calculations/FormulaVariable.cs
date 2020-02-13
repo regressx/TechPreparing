0a36c10c2a39ebebf2b01e08fc2ebcd86277adb4
RegressX <redisthepunk@gmail.com>
2020-02-13 16:51:49 +0300

    Сущность для хранения параметров из формулы

---
+++ NavisElectronics.TechPreparing/NavisElectronics.TechPreparation/Calculations/FormulaVariable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NavisElectronics.TechPreparation.Calculations
+{
+    internal class FormulaVariable
+    {
+        internal string Name { get; set; }
+
+        internal double Value { get; set; }
+
+        internal long Units { get; set; }
+
+        internal char PseudoName { get; set; }
+
+    }
+}
