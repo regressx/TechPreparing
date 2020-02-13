9fcb4d6fd732542e126b2dd652db99329f1149b2
RegressX <redisthepunk@gmail.com>
2020-02-13 16:55:58 +0300

    сервис научился получать из каталога данные по материалу

--- NavisElectronics.TechPreparing/NavisElectronics.TechPreparation/Services/RateService.cs
+++ NavisElectronics.TechPreparing/NavisElectronics.TechPreparation/Services/RateService.cs
@@ -1,9 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Data;
 using System.Globalization;
 using Intermech.Interfaces;
 using Intermech.Interfaces.Compositions;
 using Intermech.Kernel.Search;
+using Jace;
+using NavisElectronics.TechPreparation.Calculations;
 //using Jace;
 //using Jace.Execution;
 //using Jace.Operations;
@@ -18,11 +21,11 @@ namespace NavisElectronics.TechPreparation.Services
     /// </summary>
     public class RateService
     {
-        //private readonly CalculationEngine _calculationEngine;
+        private readonly CalculationEngine _calculationEngine;
 
-        public RateService()
+        public RateService(CalculationEngine calculationEngine)
         {
-            //_calculationEngine = calculationEngine;
+            _calculationEngine = calculationEngine;
         }
 
         public MeasuredValue Find(RateCatalog rateCatalog,
@@ -32,6 +35,7 @@ namespace NavisElectronics.TechPreparation.Services
                                              long operationVersionId)
         {
 
+            double result = 0d;
             // если всё правильно организовать, то поиск за log(n)
             MaterialCatalogNode material = (MaterialCatalogNode)rateCatalog.FindMaterial(materialName);
             OperationCatalogNode operationCatalog = material.FindOperation(operationName);
@@ -47,11 +51,13 @@ namespace NavisElectronics.TechPreparation.Services
                 case ActionType.GetAttributeFromOneObject:
 
                     // получить тип, который надо искать. Это может быть или операция, или заготовка
-                    int type = operationModeCatalogNode.ObjectTypeToCalculateAttribute;
+                    Guid type = operationModeCatalogNode.ObjectTypeToCalculateAttribute;
 
                     long objectTypeId = FindBlank(operationVersionId);
 
-                    // найти у этого типа атрибут и его значение
+                    char incrementChar = 'A';
+                    IList<FormulaVariable> formulaVariables = new List<FormulaVariable>();
+                    // найти у этого типа объекта атрибут и его значение. Гарантирую, что не будет более чем 26 параметров
                     foreach (string parameter in parametersToFind)
                     {
                         int attributeId = MetaDataHelper.GetAttributeByTypeNameID(parameter);
@@ -60,11 +66,33 @@ namespace NavisElectronics.TechPreparation.Services
                             IDBObject blankObject = keeper.Session.GetObject(objectTypeId);
                             IDBAttribute currentAttribute = blankObject.GetAttributeByID(attributeId);
 
+                            FormulaVariable formulaVariable = new FormulaVariable();
+                            formulaVariable.Name = parameter;
+                            if (currentAttribute!=null && currentAttribute.Value != DBNull.Value)
+                            {
+                                MeasuredValue value = (MeasuredValue)currentAttribute.Value;
+
+                                formulaVariable.Value = value.Value;
+                                formulaVariable.Units = value.MeasureID;
+                                formulaVariable.PseudoName = incrementChar;
+                                formulaVariables.Add(formulaVariable);
+                            }
                         }
+                        incrementChar++;
                     }
+
+                    foreach(FormulaVariable variable in formulaVariables)
+                    {
+                        string newFormula = operationModeCatalogNode.FormulaText.Replace("[" + variable.Name + "]", variable.Value.ToString("F6"));
+                        operationModeCatalogNode.FormulaText = newFormula;
+                    }
+
+                    result = _calculationEngine.Calculate(operationModeCatalogNode.FormulaText);
 
                     break;
             }
+
+            return new MeasuredValue(result, operationModeCatalogNode.MeasureId);
         }
 
         private long FindBlank(long operationVersionId)
@@ -79,9 +107,20 @@ namespace NavisElectronics.TechPreparation.Services
                 int type = -1;
                 long routeId = operationVersionId;
 
+
+                ColumnDescriptor[] columnsForTechRoutes =
+                {
+                new ColumnDescriptor((int) ObligatoryObjectAttributes.F_OBJECT_ID, AttributeSourceTypes.Object,
+                    ColumnContents.Text, ColumnNameMapping.Index, SortOrders.NONE, 0), // идентификатор версии объекта
+                new ColumnDescriptor((int) ObligatoryObjectAttributes.F_OBJECT_TYPE, AttributeSourceTypes.Object,
+                    ColumnContents.Text, ColumnNameMapping.Index, SortOrders.NONE, 0) // тип объекта
+            };
+
+
                 // крутим, пока не найдем маршрут обработки
                 while (type != 1037)
                 {
+                    // смотрим применяемость
                     DataTable table = compositionService.LoadCompositionApplicability(keeper.Session, routeId, 1002, columnsForTechRoutes,
                         Intermech.SystemGUIDs.filtrationBaseVersions, 1117, 1037, 1110);
 
@@ -127,28 +166,5 @@ namespace NavisElectronics.TechPreparation.Services
         }
     }
 
-    public class MaterialCatalogNode : RateCatalogNode
-    {
-        public OperationCatalogNode FindOperation(string operationName)
-        {
-            return (OperationCatalogNode)base.Find(operationName);
-        }
-    }
 
-    public class OperationCatalogNode : RateCatalogNode
-    {
-        public ModeOperationCatalogNode FindOperationMode(string operationMode)
-        {
-            return (ModeOperationCatalogNode)base.Find(operationMode);
-        }
-
-    }
-
-    public class ModeOperationCatalogNode : RateCatalogNode
-    {
-        public string FormulaText { get; set; }
-        public ActionType ActionType { get; set; }
-        public int ObjectTypeToCalculateAttribute { get; set; }
-        public long MeasureId { get; set; }
-    }
 }
