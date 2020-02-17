4b51ab655e5a874ce6f30601a74acea28d6569bb
RegressX <redisthepunk@gmail.com>
2020-02-17 20:12:56 +0300

    Теперь операция ручного электромонтажа корректно забирает данные из справочника

--- NavisElectronics.TechPreparing/NavisElectronics.TechPreparation/Services/RateService.cs
+++ NavisElectronics.TechPreparing/NavisElectronics.TechPreparation/Services/RateService.cs
@@ -34,60 +34,133 @@ namespace NavisElectronics.TechPreparation.Services
             MaterialCatalogNode material = (MaterialCatalogNode)rateCatalog.FindMaterial(materialName);
             OperationCatalogNode operationCatalog = material.FindOperation(operationName);
             ModeOperationCatalogNode operationModeCatalogNode = operationCatalog.FindOperationMode(operationMode);
-
-
-            // получить параметры из формулы
-            IEnumerable<string> parametersToFind = ParametersParser.GetParameters(operationModeCatalogNode.FormulaText);
-
+            if (operationModeCatalogNode.ObjectTypeToCalculateAttribute == new Guid("cad00167-306c-11d8-b4e9-00304f19f545"))
+            {
+                operationModeCatalogNode.ActionType = ActionType.GetFromComposition;
+            }
+            else
+            {
+                operationModeCatalogNode.ActionType = ActionType.GetAttributeFromOneObject;
+            }
             //здесь выбираем, откуда нужно забирать данные
             switch (operationModeCatalogNode.ActionType)
             {
                 case ActionType.GetAttributeFromOneObject:
 
                     // получить тип, который надо искать. Это может быть или операция, или заготовка
-                    Guid type = operationModeCatalogNode.ObjectTypeToCalculateAttribute;
+                    //Guid type = operationModeCatalogNode.ObjectTypeToCalculateAttribute;
 
-                    long objectTypeId = FindBlank(operationVersionId);
+                    long versionId = FindBlank(operationVersionId);
 
-                    char incrementChar = 'A';
-                    IList<FormulaVariable> formulaVariables = new List<FormulaVariable>();
-                    // найти у этого типа объекта атрибут и его значение. Гарантирую, что не будет более чем 26 параметров
-                    foreach (string parameter in parametersToFind)
-                    {
-                        int attributeId = MetaDataHelper.GetAttributeByTypeNameID(parameter);
-                        using (SessionKeeper keeper = new SessionKeeper())
-                        {
-                            IDBObject blankObject = keeper.Session.GetObject(objectTypeId);
-                            IDBAttribute currentAttribute = blankObject.GetAttributeByID(attributeId);
-
-                            FormulaVariable formulaVariable = new FormulaVariable();
-                            formulaVariable.Name = parameter;
-                            if (currentAttribute!=null && currentAttribute.Value != DBNull.Value)
-                            {
-                                MeasuredValue value = (MeasuredValue)currentAttribute.Value;
-
-                                formulaVariable.Value = value.Value;
-                                formulaVariable.Units = value.MeasureID;
-                                formulaVariable.PseudoName = incrementChar;
-                                formulaVariables.Add(formulaVariable);
-                            }
-                        }
-                        incrementChar++;
-                    }
-
-                    foreach(FormulaVariable variable in formulaVariables)
-                    {
-                        string newFormula = operationModeCatalogNode.FormulaText.Replace("[" + variable.Name + "]", variable.Value.ToString("F6"));
-                        operationModeCatalogNode.FormulaText = newFormula;
-                    }
-
-                    result = Convert.ToDouble(_calculationEngine.Calculate(operationModeCatalogNode.FormulaText.Replace(',','.')));
+                    result = GetRate(versionId, operationModeCatalogNode.FormulaText);
 
                     break;
+
+                case ActionType.GetFromComposition:
+                    IEnumerable<long> elements = FindAssembleUnits(operationVersionId);
+
+
+                    foreach(long id in elements)
+                    {
+                        long referenceOnObjectVersionId = 0;
+                        using (SessionKeeper keeper = new SessionKeeper())
+                        {
+                            IDBObject currentObject = keeper.Session.GetObject(id);
+                            IDBAttribute referenceOnObjectAttribute = currentObject.GetAttributeByID(1217);
+                            referenceOnObjectVersionId = (long)referenceOnObjectAttribute.Value;
+                        }
+                        double currentRate = GetRate(referenceOnObjectVersionId, operationModeCatalogNode.FormulaText);
+
+                        result += currentRate;
+                    }
+
+                    break;
+
             }
 
             return new MeasuredValue(result, operationModeCatalogNode.MeasureId);
         }
+
+
+        private double GetRate(long versionId, string formulaText)
+        {
+            // получить параметры из формулы
+            IEnumerable<string> parametersToFind = ParametersParser.GetParameters(formulaText);
+
+            char incrementChar = 'A';
+            IList<FormulaVariable> formulaVariables = new List<FormulaVariable>();
+            
+            // найти у этого типа объекта атрибут и его значение. Гарантирую, что не будет более чем 26 параметров
+            foreach (string parameter in parametersToFind)
+            {
+                int attributeId = MetaDataHelper.GetAttributeByTypeNameID(parameter);
+                using (SessionKeeper keeper = new SessionKeeper())
+                {
+                    IDBObject currentObject = keeper.Session.GetObject(versionId);
+                    IDBAttribute currentAttribute = currentObject.GetAttributeByID(attributeId);
+
+                    FormulaVariable formulaVariable = new FormulaVariable();
+                    formulaVariable.Name = parameter;
+                    if (currentAttribute != null && currentAttribute.Value != DBNull.Value)
+                    {
+                        MeasuredValue value = null;
+                        try
+                        {
+                            value = (MeasuredValue)currentAttribute.Value;
+                        }
+                        catch(InvalidCastException)
+                        {
+                            value = new MeasuredValue(Convert.ToDouble(currentAttribute.Value), 2804);
+                        }
+
+                        formulaVariable.Value = value.Value;
+                        formulaVariable.PseudoName = incrementChar;
+                        formulaVariables.Add(formulaVariable);
+                    }
+                }
+                incrementChar++;
+            }
+
+            foreach (FormulaVariable variable in formulaVariables)
+            {
+                string newFormula = formulaText.Replace("[" + variable.Name + "]", variable.Value.ToString("F6",CultureInfo.InvariantCulture));
+                formulaText = newFormula;
+            }
+
+            return Convert.ToDouble(_calculationEngine.Calculate(formulaText));
+
+        }
+
+
+
+        private IEnumerable<long> FindAssembleUnits(long operationId)
+        {
+            ICollection<long> collection = new List<long>();
+
+            using (SessionKeeper keeper = new SessionKeeper())
+            {
+                // Сервис для получения составов
+                ICompositionLoadService compositionService =
+                    (ICompositionLoadService)keeper.Session.GetCustomService(typeof(ICompositionLoadService));
+
+                ColumnDescriptor[] columnsForOperationComposition =
+{
+                    new ColumnDescriptor((int) ObligatoryObjectAttributes.F_OBJECT_ID, AttributeSourceTypes.Object,
+                        ColumnContents.Text, ColumnNameMapping.Index, SortOrders.NONE, 0), // идентификатор версии объекта
+                };
+
+                DataTable assemblyUnitsTable = compositionService.LoadComposition(keeper.Session, operationId, 1002, new List<ColumnDescriptor>(columnsForOperationComposition), Intermech.SystemGUIDs.filtrationBaseVersions, 1201); // забрать собираемые единицы
+
+                foreach (DataRow row in assemblyUnitsTable.Rows)
+                {
+                    collection.Add((long)row[0]);
+                }
+            }
+
+            return collection;
+
+        }
+
 
         private long FindBlank(long operationVersionId)
         {
@@ -104,11 +177,11 @@ namespace NavisElectronics.TechPreparation.Services
 
                 ColumnDescriptor[] columnsForTechRoutes =
                 {
-                new ColumnDescriptor((int) ObligatoryObjectAttributes.F_OBJECT_ID, AttributeSourceTypes.Object,
-                    ColumnContents.Text, ColumnNameMapping.Index, SortOrders.NONE, 0), // идентификатор версии объекта
-                new ColumnDescriptor((int) ObligatoryObjectAttributes.F_OBJECT_TYPE, AttributeSourceTypes.Object,
-                    ColumnContents.Text, ColumnNameMapping.Index, SortOrders.NONE, 0) // тип объекта
-            };
+                    new ColumnDescriptor((int) ObligatoryObjectAttributes.F_OBJECT_ID, AttributeSourceTypes.Object,
+                        ColumnContents.Text, ColumnNameMapping.Index, SortOrders.NONE, 0), // идентификатор версии объекта
+                    new ColumnDescriptor((int) ObligatoryObjectAttributes.F_OBJECT_TYPE, AttributeSourceTypes.Object,
+                        ColumnContents.Text, ColumnNameMapping.Index, SortOrders.NONE, 0) // тип объекта
+                };
 
 
                 // крутим, пока не найдем маршрут обработки
