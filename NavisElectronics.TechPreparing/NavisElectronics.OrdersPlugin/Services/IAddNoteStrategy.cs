c104de5e721bc653878893d7ae2329f151aaa71b
RegressX <redisthepunk@gmail.com>
2020-02-19 11:34:35 +0300

    стратегия добавления примечания

---
+++ NavisElectronics.TechPreparing/NavisElectronics.OrdersPlugin/Services/IAddNoteStrategy.cs
@@ -0,0 +1,57 @@
+﻿using NavisElectronics.TechPreparation.Interfaces.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NavisElectronics.Orders.Services
+{
+    public interface IAddNoteStrategy
+    {
+        void AddNote(IntermechTreeElement element, string note);
+    }
+
+    internal class AddNoteToThisNodeStrategy : IAddNoteStrategy
+    {
+        public void AddNote(IntermechTreeElement element, string note)
+        {
+            string oldNote = element.Note == null ? string.Empty : element.Note;
+            element.Note = string.Format($"{note} {oldNote}"); 
+        }
+    }
+
+    internal class AddNoteToWholeTreeStrategy : IAddNoteStrategy
+    {
+        public void AddNote(IntermechTreeElement element, string note)
+        {
+            // дойти до корня
+            IntermechTreeElement root = element;
+            while(root.Parent!=null)
+            {
+                root = root.Parent;
+            }
+
+            // найти все вхождения узла во всём дереве
+            Queue<IntermechTreeElement> queue = new Queue<IntermechTreeElement>();
+            queue.Enqueue(root);
+
+            while (queue.Count > 0)
+            {
+                IntermechTreeElement elementFromQueue = queue.Dequeue();
+
+                if (elementFromQueue.ObjectId == element.ObjectId)
+                {
+                    string oldNote = elementFromQueue.Note == null ? string.Empty : elementFromQueue.Note;
+                    elementFromQueue.Note = string.Format($"{note} {oldNote}");
+                }
+
+                foreach(IntermechTreeElement child in elementFromQueue.Children)
+                {
+                    queue.Enqueue(child);
+                }
+
+            }
+        }
+    }
+}
